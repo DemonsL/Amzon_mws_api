d7384f7a135ffd173b6471ee5c4f58bec86cbbdf
luomurong <coolpad@yulong.com>
2019-06-10 18:33:21 +0800

    Add common method

--- Common/common.py
+++ Common/common.py
@@ -2,8 +2,11 @@
 import json
 import pytz
 import datetime
+import logging
 import xmltodict
 from Config import mws_config
+
+log = logging.getLogger()
 
 def get_client(client_obj, mkp):
     access_key = mws_config.client.get('access_key')
@@ -27,7 +30,7 @@ def amz_iso_time(time):
 
 def iso_time_to_date(time_str):
     if time_str:
-        time_date = datetime.datetime.fromisoformat(time_str)
+        time_date = datetime.datetime.fromisoformat(time_str[:-1])
         return time_date
 
 def iso_time_to_dsttime(time_str):
@@ -36,6 +39,22 @@ def iso_time_to_dsttime(time_str):
         dst_zone = pytz.timezone('US/Pacific')
         dsttime = time_date.astimezone(dst_zone)
         return dsttime.strftime('%Y-%m-%d %H:%M:%S')
+
+def dsttime_to_utctime(time_str):
+    if time_str:
+        time_date = datetime.datetime.strptime(time_str, '%Y-%m-%d %H:%M:%S')
+        us_date = pytz.timezone('US/Pacific').localize(time_date)
+        utc_zone = pytz.timezone('UTC')
+        utctime = us_date.astimezone(utc_zone)
+        return utctime
+
+def utctime_to_dsttime(time_str):
+    if time_str:
+        time_date = iso_time_to_date(time_str)
+        utc_date = pytz.timezone('UTC').localize(time_date)
+        us_zone = pytz.timezone('US/Pacific')
+        us_time = utc_date.astimezone(us_zone)
+        return us_time
 
 def xml_to_json(data):
     data_dict = xmltodict.parse(data)
@@ -58,3 +77,12 @@ def set_param_value(param):
     if param == 'Infinite':
         resp = 99999.9999
     return resp
+
+def is_value(field_keys, field_dict, default_value):
+    resp = default_value
+    field_key1 = field_keys.split('_')[0]
+    field_key2 = field_keys.split('_')[1]
+    if field_key1 in field_dict.keys():
+        resp = field_dict.get(field_key1).get(field_key2)
+    return resp
+
