e31ad50c62df623d53368c8ff1ea5bb67393046a
luomurong <coolpad@yulong.com>
2019-06-10 18:36:15 +0800

    Add order model and download client

---
+++ Models/orders.py
@@ -0,0 +1,205 @@
+# coding: utf-8
+from Config import db
+from Common import common
+from sqlalchemy import Column, String, Integer, Float, DECIMAL, DateTime, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+DBConnection = 'mysql+pymysql://%s:%s@%s:%s/%s?charset=%s' % \
+               (db.User, db.Passwd, db.Host, db.Port, db.DB, db.CharSet)
+engine = create_engine(DBConnection)
+DBSession = sessionmaker(bind=engine)
+
+
+Base = declarative_base()
+class BkAmzOrder(Base):
+
+    __tablename__ = 'BK_AMZ_ORDER'
+
+    AmazonOrderId = Column(String(20), primary_key=True)
+    BuyerEmail = Column(String(50))
+    BuyerName = Column(String(50))
+    EarliestShipDate = Column(DateTime)
+    FulfillmentChannel = Column(String(10))
+    IsBusinessOrder = Column(String(10))
+    IsPremiumOrder = Column(String(10))
+    IsPrime = Column(String(10))
+    IsReplacementOrder = Column(String(10))
+    LastUpdateDate = Column(DateTime)
+    LatestShipDate = Column(DateTime)
+    MarketplaceId = Column(String(40))
+    NumberOfItemsShipped = Column(Integer)
+    NumberOfItemsUnshipped = Column(Integer)
+    OrderStatus = Column(String(20))
+    OrderAmount = Column(Float(10,2))
+    OrderCurrencyCode = Column(String(15))
+    OrderType = Column(String(20))
+    PaymentMethod = Column(String(20))
+    PaymentMethodDetail = Column(String(20))
+    PurchaseDate = Column(DateTime)
+    SalesChannel = Column(String(30))
+    ShipServiceLevel = Column(String(20))
+    SellerOrderId = Column(String(30))
+    ShipmentServiceLevelCategory = Column(String(20))
+    CountryCode = Column(String(20))
+    ShippingStateOrRegion = Column(String(30))
+    City = Column(String(40))
+    PostalCode = Column(String(25))
+    AddressLine_1 = Column(String(200))
+    AddressLine_2 = Column(String(200))
+    AddressLine_3 = Column(String(200))
+    AddresseeName = Column(String(200))
+    EmailMark = Column(String(10))
+
+    def __init__(self, json_order):
+        self.AmazonOrderId = json_order.get('AmazonOrderId')
+        self.BuyerEmail = json_order.get('BuyerEmail')
+        self.BuyerName = json_order.get('BuyerName')
+        self.EarliestShipDate = common.utctime_to_dsttime(json_order.get('EarliestShipDate'))
+        self.FulfillmentChannel = json_order.get('FulfillmentChannel')
+        self.IsBusinessOrder = json_order.get('IsBusinessOrder')
+        self.IsPremiumOrder = json_order.get('IsPremiumOrder')
+        self.IsPrime = json_order.get('IsPrime')
+        self.IsReplacementOrder = json_order.get('IsReplacementOrder')
+        self.LastUpdateDate = common.utctime_to_dsttime(json_order.get('LastUpdateDate'))
+        self.LatestShipDate = common.utctime_to_dsttime(json_order.get('LatestShipDate'))
+        self.MarketplaceId = json_order.get('MarketplaceId')
+        self.NumberOfItemsShipped = json_order.get('NumberOfItemsShipped')
+        self.NumberOfItemsUnshipped = json_order.get('NumberOfItemsUnshipped')
+        self.OrderStatus = json_order.get('OrderStatus')
+        self.OrderAmount = common.is_value('OrderTotal_Amount', json_order, 0)
+        self.OrderCurrencyCode = common.is_value('OrderTotal_CurrencyCode', json_order, '')
+        self.OrderType = json_order.get('OrderType')
+        self.PaymentMethod = json_order.get('PaymentMethod')
+        self.PaymentMethodDetail = json_order.get('PaymentMethodDetails').get('PaymentMethodDetail')
+        self.PurchaseDate = common.utctime_to_dsttime(json_order.get('PurchaseDate'))
+        self.SalesChannel = json_order.get('SalesChannel')
+        self.ShipServiceLevel = json_order.get('ShipServiceLevel')
+        self.SellerOrderId = json_order.get('SellerOrderId')
+        self.ShipmentServiceLevelCategory = json_order.get('ShipmentServiceLevelCategory')
+        self.CountryCode = common.is_value('ShippingAddress_CountryCode', json_order, '')
+        self.ShippingStateOrRegion = common.is_value('ShippingAddress_StateOrRegion', json_order, '')
+        self.City = common.is_value('ShippingAddress_City', json_order, '')
+        self.PostalCode = common.is_value('ShippingAddress_PostalCode', json_order, '')
+        self.AddressLine_1 = common.is_value('ShippingAddress_AddressLine1', json_order, '')
+        self.AddressLine_2 = common.is_value('ShippingAddress_AddressLine2', json_order, '')
+        self.AddressLine_3 = common.is_value('ShippingAddress_AddressLine3', json_order, '')
+        self.AddresseeName = common.is_value('ShippingAddress_Name', json_order, '')
+
+
+
+class BkAmzOrderItem(Base):
+
+    __tablename__ = 'BK_AMZ_ORDER_ITEM'
+
+    OrderItemId = Column(String(40), primary_key=True)
+    ASIN = Column(String(40))
+    SellerSKU = Column(String(40))
+    Title = Column(String(300))
+    NumberOfItems = Column(Integer)
+    AmazonOrderId = Column(String(20))
+    QuantityOrdered = Column(Integer)
+    QuantityShipped = Column(Integer)
+    IsGift = Column(String(5))
+    ItemPrice_Amount = Column(Float(10,2))
+    ItemPrice_CurrencyCode = Column(String(10))
+    ItemTax_Amount = Column(Float(10))
+    ItemTax_CurrencyCode = Column(String(10))
+    PromotionDiscount_Amount = Column(Float(10,2))
+    PromotionDiscount_CurrencyCode = Column(String(10))
+    ShippingPrice_Amount = Column(Float(10,2))
+    ShippingPrice_CurrencyCode = Column(String(10))
+    ShippingTax_Amount = Column(Float(10,2))
+    ShippingTax_CurrencyCode = Column(String(10))
+    ShippingDiscount_Amount = Column(Float(10,2))
+    ShippingDiscount_CurrencyCode = Column(String(10))
+    PurchaseDate = Column(DateTime)
+
+    def __init__(self, order_id, order_time, json_order_item):
+        self.OrderItemId = json_order_item.get('OrderItemId')
+        self.ASIN = json_order_item.get('ASIN')
+        self.SellerSKU = json_order_item.get('SellerSKU')
+        self.Title = json_order_item.get('Title')
+        self.NumberOfItems = json_order_item.get('ProductInfo').get('NumberOfItems')
+        self.AmazonOrderId = order_id
+        self.QuantityOrdered = json_order_item.get('QuantityOrdered')
+        self.QuantityShipped = json_order_item.get('QuantityShipped')
+        self.IsGift = json_order_item.get('IsGift')
+        self.ItemPrice_Amount = json_order_item.get('ItemPrice').get('Amount')
+        self.ItemPrice_CurrencyCode = json_order_item.get('ItemPrice').get('CurrencyCode')
+        self.ItemTax_Amount = json_order_item.get('ItemTax').get('Amount')
+        self.ItemTax_CurrencyCode = json_order_item.get('ItemTax').get('CurrencyCode')
+        self.PromotionDiscount_Amount = json_order_item.get('PromotionDiscount').get('Amount')
+        self.PromotionDiscount_CurrencyCode = json_order_item.get('PromotionDiscount').get('CurrencyCode')
+        self.ShippingPrice_Amount = common.is_value('ShippingPrice_Amount', json_order_item, 0)
+        self.ShippingPrice_CurrencyCode = common.is_value('ShippingPrice_CurrencyCode', json_order_item, '')
+        self.ShippingTax_Amount = common.is_value('ShippingTax_Amount', json_order_item, 0)
+        self.ShippingTax_CurrencyCode = common.is_value('ShippingTax_CurrencyCode', json_order_item, '')
+        self.ShippingDiscount_Amount = common.is_value('ShippingDiscount_Amount', json_order_item, 0)
+        self.ShippingDiscount_CurrencyCode = common.is_value('ShippingDiscount_CurrencyCode', json_order_item, '')
+        self.PurchaseDate = order_time
+
+
+
+
+
+def update_order(json_order):
+    update_field = {
+        'BuyerEmail' : json_order.get('BuyerEmail'),
+        'BuyerName' : json_order.get('BuyerName'),
+        'EarliestShipDate' : common.utctime_to_dsttime(json_order.get('EarliestShipDate')),
+        'FulfillmentChannel' : json_order.get('FulfillmentChannel'),
+        'IsBusinessOrder' : json_order.get('IsBusinessOrder'),
+        'IsPremiumOrder' : json_order.get('IsPremiumOrder'),
+        'IsPrime' : json_order.get('IsPrime'),
+        'IsReplacementOrder' : json_order.get('IsReplacementOrder'),
+        'LastUpdateDate' : common.utctime_to_dsttime(json_order.get('LastUpdateDate')),
+        'LatestShipDate' : common.utctime_to_dsttime(json_order.get('LatestShipDate')),
+        'MarketplaceId' : json_order.get('MarketplaceId'),
+        'NumberOfItemsShipped' : json_order.get('NumberOfItemsShipped'),
+        'NumberOfItemsUnshipped' : json_order.get('NumberOfItemsUnshipped'),
+        'OrderStatus' : json_order.get('OrderStatus'),
+        'OrderAmount' : common.is_value('OrderTotal_Amount', json_order, 0),
+        'OrderCurrencyCode' : common.is_value('OrderTotal_CurrencyCode', json_order, ''),
+        'OrderType' : json_order.get('OrderType'),
+        'PaymentMethod' : json_order.get('PaymentMethod'),
+        'PaymentMethodDetail' : json_order.get('PaymentMethodDetails').get('PaymentMethodDetail'),
+        'PurchaseDate' : common.utctime_to_dsttime(json_order.get('PurchaseDate')),
+        'SalesChannel' : json_order.get('SalesChannel'),
+        'ShipServiceLevel' : json_order.get('ShipServiceLevel'),
+        'SellerOrderId' : json_order.get('SellerOrderId'),
+        'ShipmentServiceLevelCategory' : json_order.get('ShipmentServiceLevelCategory'),
+        'CountryCode' : common.is_value('ShippingAddress_CountryCode', json_order, ''),
+        'ShippingStateOrRegion' : common.is_value('ShippingAddress_StateOrRegion', json_order, ''),
+        'City' : common.is_value('ShippingAddress_City', json_order, ''),
+        'PostalCode' : common.is_value('ShippingAddress_PostalCode', json_order, ''),
+        'AddressLine_1' : common.is_value('ShippingAddress_AddressLine1', json_order, ''),
+        'AddressLine_2' : common.is_value('ShippingAddress_AddressLine2', json_order, ''),
+        'AddressLine_3' : common.is_value('ShippingAddress_AddressLine3', json_order, ''),
+        'AddresseeName' : common.is_value('ShippingAddress_Name', json_order, '')
+    }
+    return update_field
+
+def update_order_item(json_order_item):
+    update_field = {
+        'ASIN' : json_order_item.get('ASIN'),
+        'SellerSKU' : json_order_item.get('SellerSKU'),
+        'Title' : json_order_item.get('Title'),
+        'NumberOfItems' : json_order_item.get('ProductInfo').get('NumberOfItems'),
+        'QuantityOrdered' : json_order_item.get('QuantityOrdered'),
+        'QuantityShipped' : json_order_item.get('QuantityShipped'),
+        'IsGift' : json_order_item.get('IsGift'),
+        'ItemPrice_Amount' : json_order_item.get('ItemPrice').get('Amount'),
+        'ItemPrice_CurrencyCode' : json_order_item.get('ItemPrice').get('CurrencyCode'),
+        'ItemTax_Amount' : json_order_item.get('ItemTax').get('Amount'),
+        'ItemTax_CurrencyCode' : json_order_item.get('ItemTax').get('CurrencyCode'),
+        'PromotionDiscount_Amount' : json_order_item.get('PromotionDiscount').get('Amount'),
+        'PromotionDiscount_CurrencyCode' : json_order_item.get('PromotionDiscount').get('CurrencyCode'),
+        'ShippingPrice_Amount' : common.is_value('ShippingPrice_Amount', json_order_item, 0),
+        'ShippingPrice_CurrencyCode' : common.is_value('ShippingPrice_CurrencyCode', json_order_item, ''),
+        'ShippingTax_Amount' : common.is_value('ShippingTax_Amount', json_order_item, 0),
+        'ShippingTax_CurrencyCode' : common.is_value('ShippingTax_CurrencyCode', json_order_item, ''),
+        'ShippingDiscount_Amount' : common.is_value('ShippingDiscount_Amount', json_order_item, 0),
+        'ShippingDiscount_CurrencyCode' : common.is_value('ShippingDiscount_CurrencyCode', json_order_item, '')
+    }
+    return update_field
---
+++ Timers/download_orders.py
@@ -0,0 +1,210 @@
+# coding: utf-8
+import sys
+sys.path.append('../')
+import logging
+import datetime
+from Common import common
+from Models import orders
+from MwsApi.orders import Orders
+from sqlalchemy import desc
+
+
+formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+file_name = '/home/develop/logs/mws_orders/{}.log'.format(datetime.date.today())
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+
+
+fh = logging.FileHandler(file_name, mode='w')
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(formatter)
+log.addHandler(fh)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+ch.setFormatter(formatter)
+log.addHandler(ch)
+
+class DownloadOrders:
+
+    def list_orders(self, od_client, params):
+        resp = []
+        try:
+            resp_xml = od_client.list_orders(params)
+            resp = common.xml_to_json(resp_xml.text)
+        except Exception as e:
+            log.error('ListOrdersError: %s', e)
+        return resp
+
+    def list_orders_by_next_token(self, od_client, params):
+        resp = []
+        try:
+            resp_xml = od_client.list_orders_by_next_token(params)
+            resp = common.xml_to_json(resp_xml.text)
+        except Exception as e:
+            log.error('ListOrdersByNextTokenError: %s', e)
+        return resp
+
+    def get_order(self, od_client, params):
+        resp = []
+        try:
+            resp_xml = od_client.get_order(params)
+            resp = common.xml_to_json(resp_xml.text)
+        except Exception as e:
+            log.error('GetOrderError: %s', e)
+        return resp
+
+    def list_order_items(self, od_client, params):
+        resp = []
+        try:
+            resp_xml = od_client.list_order_items(params)
+            resp = common.xml_to_json(resp_xml.text)
+        except Exception as e:
+            log.error('ListOrderItemsError: %s', e)
+        return resp
+
+    def list_order_items_by_next_token(self, od_client, params):
+        resp = []
+        try:
+            resp_xml = od_client.list_order_items_by_next_token(params)
+            resp = common.xml_to_json(resp_xml.text)
+        except Exception as e:
+            log.error('ListOrderItemsByNextTokenError: %s', e)
+        return resp
+
+    def select_last_order_time(self):
+        session = orders.DBSession()
+        field_name = orders.BkAmzOrder.LastUpdateDate
+        last_order_time = session.query(orders.BkAmzOrder, field_name).order_by(desc(field_name)).limit(1).one()
+        session.close()
+        return last_order_time[1]
+
+    def select_order_ids(self):
+        session = orders.DBSession()
+        order_ids = session.query(orders.BkAmzOrder, orders.BkAmzOrder.AmazonOrderId).all()
+        order_ids = [order_id[1] for order_id in order_ids]
+        session.close()
+        return order_ids
+
+    def select_order_item_ids(self, order_id):
+        session = orders.DBSession()
+        item_ids = session.query(orders.BkAmzOrderItem, orders.BkAmzOrderItem.OrderItemId) \
+                          .filter_by(AmazonOrderId=order_id).all()
+        item_ids = [item_id[1] for item_id in item_ids]
+        session.close()
+        return item_ids
+
+    def select_order_time(self, order_id):
+        session = orders.DBSession()
+        order_time = session.query(orders.BkAmzOrder, orders.BkAmzOrder.PurchaseDate) \
+                            .filter_by(AmazonOrderId=order_id).one()
+        session.close()
+        return order_time[1]
+
+    def add_order_to_sql(self, json_order):
+        session = orders.DBSession()
+        session.add(orders.BkAmzOrder(json_order))
+        session.commit()
+        session.close()
+
+    def add_order_item(self, order_id, order_time, json_order_item):
+        session = orders.DBSession()
+        session.add(orders.BkAmzOrderItem(order_id, order_time, json_order_item)),
+        session.commit()
+        session.close()
+
+    def update_order_to_sql(self, order_id, json_order):
+        session = orders.DBSession()
+        session.query(orders.BkAmzOrder).filter_by(AmazonOrderId=order_id) \
+                                        .update(orders.update_order(json_order))
+        session.commit()
+        session.close()
+
+    def update_order_item(self, item_id, json_order_item):
+        session = orders.DBSession()
+        session.query(orders.BkAmzOrderItem).filter_by(OrderItemId=item_id) \
+                                            .update(orders.update_order_item(json_order_item))
+        session.commit()
+        session.close()
+
+
+
+
+
+
+
+def order_item_to_sql(dw_meth, order_item, db_order_item, order_id, order_time):
+    order_item_id = order_item.get('OrderItemId')
+    if order_item_id in db_order_item:
+        log.info('Update order item: %s', order_item_id)
+        dw_meth.update_order_item(order_item_id, order_item)
+    else:
+        log.info('Add order item: %s', order_item_id)
+        dw_meth.add_order_item(order_id, order_time, order_item)
+
+def download_order_item_start(order_id, dw_meth):
+    order_item_params = {
+        'AmazonOrderId': order_id
+    }
+    order_time = dw_meth.select_order_time(order_id)
+    db_order_item_ids = dw_meth.select_order_item_ids(order_id)
+    order_item_resp = dw_meth.list_order_items(od_client, order_item_params)
+
+    order_items = order_item_resp.get('ListOrderItemsResponse') \
+                                 .get('ListOrderItemsResult') \
+                                 .get('OrderItems') \
+                                 .get('OrderItem')
+    if not isinstance(order_items, list):
+        order_item_to_sql(dw_meth, order_items, db_order_item_ids, order_id, order_time)
+    else:
+        for order_item in order_items:
+            order_item_to_sql(dw_meth, order_item, db_order_item_ids, order_id, order_time)
+
+def download_order_start(dw_meth, order_resp, db_order):
+    list_orders = order_resp.get('ListOrdersResponse').get('ListOrdersResult').get('Orders')
+    list_order = list_orders.get('Order')
+    order_next_token = list_orders.get('NextToken', None)
+
+    for order in list_order:
+        order_id = order.get('AmazonOrderId')
+        if order_id in db_order:
+            log.info('Update order: %s', order_id)
+            dw_meth.update_order_to_sql(order_id, order)
+            download_order_item_start(order_id, dw_meth)
+        else:
+            log.info('Add order: %s', order_id)
+            dw_meth.add_order_to_sql(order)
+            download_order_item_start(order_id, dw_meth)
+    return order_next_token
+
+
+
+
+if __name__ == '__main__':
+
+    dw_orders = DownloadOrders()
+    od_client = common.get_client(Orders, 'us')
+
+    last_update = dw_orders.select_last_order_time()
+    local_time = common.dsttime_to_utctime(str(last_update))
+    order_params = {
+        'LastUpdatedAfter': local_time
+    }
+    log.info(order_params)
+    db_order_ids = dw_orders.select_order_ids()
+
+    order_resp = dw_orders.list_orders(od_client, order_params)
+    next_token = download_order_start(dw_orders, order_resp, db_order_ids)
+    while next_token:
+        next_params = {
+            'NextToken': next_token
+        }
+        log.info(next_params)
+        next_resp = dw_orders.list_orders_by_next_token(od_client, next_params)
+        next_token = download_order_start(dw_orders, next_resp, db_order_ids)
+
+
+
+
+
+
