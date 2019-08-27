1db1bd06792399fbcfa2e628c8e5dde7a15f03a1
luomurong <coolpad@yulong.com>
2019-08-27 16:35:10 +0800

    Update report api download bak

--- Timers/download_reports.py
+++ Timers/download_reports.py
@@ -109,16 +109,17 @@ class DownloadReports:
                     return
                 rp = self.get_report(rp_client, rp_id)
 
-                if params.get('ReportType') == '_GET_FBA_FULFILLMENT_CUSTOMER_RETURNS_DATA_':
-                    log.info('Report fba_return bak')
-                    rp_return_name = '/home/develop/Mws_reports/fba_return/{}.json'.format(
-                                     str(params.get('StartDate')).split(' ')[0])
-                    with open(rp_return_name, 'w', encoding='utf-8') as f:
-                        f.write(str(rp))
+                tb_name = params.get('table_name')
+                log.info('Report fba_return bak')
+                rp_return_name = '/home/develop/Mws_reports/{f_path}/{f_name}.json'.format(
+                                 f_path=tb_name,
+                                 f_name=str(params.get('StartDate')).split(' ')[0])
+                with open(rp_return_name, 'w', encoding='utf-8') as f:
+                    f.write(str(rp))
 
                 log.info('Report add to sql...')
                 mkp = params.get('mkp')
-                tb_name = params.get('table_name')
+
                 rp_date = datetime.datetime.strptime(datetime.datetime.now().strftime('%Y-%m-%d'), '%Y-%m-%d')
                 try:
                     if tb_name in ['AprFBAAllOrders', 'AprFBAShipments', 'AprFBAInventoryAge',
