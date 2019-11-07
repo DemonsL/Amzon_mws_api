e777c790c3a5b8c0d432510a3ca317c9c1f93c14
luomurong <coolpad@yulong.com>
2019-11-07 16:16:34 +0800

    Add product rank api

---
+++ Models/products.py
@@ -0,0 +1,131 @@
+# coding: utf-8
+from Config import db
+from Common import common
+from sqlalchemy import Column, String, Integer, Float, DECIMAL, DateTime, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+DBConnection = 'mysql+pymysql://%s:%s@%s:%s/%s?charset=%s' % \
+               (db.User, db.Passwd, db.Host, db.Port, db.DB, db.CharSet)
+engine = create_engine(DBConnection)
+DBSession = sessionmaker(bind=engine)
+
+
+Base = declarative_base()
+class AprProductInfo(Base):
+
+    __tablename__ = 'Apr_Product_Info'
+
+    ID = Column(Integer, primary_key=True)
+    Country = Column(String(10))
+    SnapDate = Column(DateTime)
+    Asin = Column(String(20))
+    Brand = Column(String(100))
+    ListingDate = Column(DateTime)
+    ParentAsin = Column(String(50))
+    Color = Column(String(50))
+    MaterialType = Column(String(100))
+    Pack = Column(Integer)
+    Size = Column(String(200))
+    Title = Column(String(200))
+    SmallImageUrl = Column(String(200))
+    Length = Column(DECIMAL(6,1))
+    Width = Column(DECIMAL(6,1))
+    Height = Column(DECIMAL(6,1))
+    Weight = Column(DECIMAL(6,2))
+    PackageLength = Column(DECIMAL(6,1))
+    PackageWidth = Column(DECIMAL(6,1))
+    PackageHeight = Column(DECIMAL(6,1))
+    PackageWeight = Column(DECIMAL(6,2))
+
+    def __init__(self, country, snap_date, json_product):
+        self.Country = country
+        self.SnapDate = snap_date
+        self.Asin = json_product.get('Identifiers').get('MarketplaceASIN').get('ASIN')
+        self.Brand = json_product.get('AttributeSets').get('ns2:ItemAttributes').get('ns2:Brand')
+        self.ListingDate = json_product.get('AttributeSets').get('ns2:ItemAttributes').get('ns2:ReleaseDate')
+        self.ParentAsin = json_product.get('Relationships').get('VariationParent').get('Identifiers') \
+                                      .get('MarketplaceASIN').get('ASIN') if not json_product.get('Relationships') else ''
+        self.Color = json_product.get('AttributeSets').get('ItemAttributes').get('ns2:Color')
+        self.MaterialType = json_product.get('AttributeSets').get('ItemAttributes').get('ns2:MaterialType')
+        self.Pack = json_product.get('AttributeSets').get('ItemAttributes').get('ns2:PackageQuantity')
+        self.Size = json_product.get('AttributeSets').get('ItemAttributes').get('ns2:Size')
+        self.Title = json_product.get('AttributeSets').get('ItemAttributes').get('ns2:Title')
+        self.SmallImageUrl = json_product.get('AttributeSets').get('ItemAttributes').get('ns2:SmallImage').get('ns2:URL')
+        self.Length = json_product.get('AttributeSets').get('ItemAttributes').get('ns2:ItemDimensions') \
+                                  .get('ns2:Length').get('#text')
+        self.Width = json_product.get('AttributeSets').get('ItemAttributes').get('ns2:ItemDimensions') \
+                                 .get('ns2:Width').get('#text')
+        self.Height = json_product.get('AttributeSets').get('ItemAttributes').get('ns2:ItemDimensions') \
+                                  .get('ns2:Height').get('#text')
+        self.Weight = json_product.get('AttributeSets').get('ItemAttributes').get('ns2:ItemDimensions') \
+                                  .get('ns2:Weight').get('#text')
+        self.PackageLength = json_product.get('AttributeSets').get('ItemAttributes').get('ns2:PackageDimensions') \
+                                         .get('ns2:Length').get('#text')
+        self.PackageWidth = json_product.get('AttributeSets').get('ItemAttributes').get('ns2:PackageDimensions') \
+                                        .get('ns2:Width').get('#text')
+        self.PackageHeight = json_product.get('AttributeSets').get('ItemAttributes').get('ns2:PackageDimensions') \
+                                         .get('ns2:Height').get('#text')
+        self.PackageWeight = json_product.get('AttributeSets').get('ItemAttributes').get('ns2:PackageDimensions') \
+                                         .get('ns2:Weight').get('#text')
+
+
+class AprProductDaily(Base):
+
+    __tablename__ = 'Apr_Product_Daily'
+
+    ID = Column(Integer, primary_key=True)
+    Country = Column(String(10))
+    SnapDate = Column(DateTime)
+    Asin = Column(String(20))
+    Title = Column(String(200))
+    ProductCategory = Column(String(60))
+    Category1 = Column(String(100))
+    Rank1 = Column(Integer)
+    Category2 = Column(String(100))
+    Rank2 = Column(Integer)
+    Category3 = Column(String(100))
+    Rank3 = Column(Integer)
+
+    def __init__(self, country, snap_date, json_product):
+        self.Country = country
+        self.SnapDate = snap_date
+        self.Asin = json_product.get('Identifiers').get('MarketplaceASIN').get('ASIN')
+        self.Title = json_product.get('AttributeSets').get('ItemAttributes').get('ns2:Title')
+        self.ProductCategory = json_product.get('AttributeSets').get('ItemAttributes').get('ns2:ProductGroup')
+        self.Category1 = json_product.get('SalesRankings').get('SalesRank')[0].get('ProductCategoryId')
+        self.Rank1 = json_product.get('SalesRankings').get('SalesRank')[0].get('Rank')
+        self.Category2 = json_product.get('SalesRankings').get('SalesRank')[1].get('ProductCategoryId')
+        self.Rank2 = json_product.get('SalesRankings').get('SalesRank')[1].get('Rank')
+        self.Category3 = json_product.get('SalesRankings').get('SalesRank')[2].get('ProductCategoryId') \
+                                       if json_product.get('SalesRankings').get('SalesRank')[2] else ''
+        self.Rank3 = json_product.get('SalesRankings').get('SalesRank')[2].get('Rank') \
+                                       if json_product.get('SalesRankings').get('SalesRank')[2] else 0
+
+
+class PubAsin(Base):
+
+    __tablename__ = 'Pub_Asin'
+
+    Asin = Column(String(20), primary_key=True)
+
+
+class AprAsinRank(Base):
+
+    __tablename__ = 'Apr_Asin_Rank'
+
+    ID = Column(Integer)
+    Country = Column(String(10), primary_key=True)
+    SnapDate = Column(DateTime, primary_key=True)
+    Asin = Column(String(20), primary_key=True)
+    CategoryId = Column(String(100), primary_key=True)
+    Rank = Column(Integer)
+    LastUpdate = Column(DateTime)
+
+    def __init__(self, country, snap_date, asin, json_rank):
+        self.Country = country
+        self.SnapDate = snap_date.split(' ')[0]
+        self.Asin = asin
+        self.CategoryId = json_rank.get('ProductCategoryId')
+        self.Rank = json_rank.get('Rank')
+        self.LastUpdate = snap_date
---
+++ Timers/download_products.py
@@ -0,0 +1,177 @@
+# coding: utf-8
+import os, sys
+sys.path.append('../')
+import time
+import logging
+import datetime
+from Common import common
+from Models import products
+from MwsApi.products import Products
+from sqlalchemy import desc, and_
+
+
+formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+file_name = '/home/develop/logs/mws_products/{}.log'.format(datetime.date.today())
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+
+
+fh = logging.FileHandler(file_name, mode='a+')
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(formatter)
+log.addHandler(fh)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+ch.setFormatter(formatter)
+log.addHandler(ch)
+
+class DownloadProducts:
+
+    def list_matching_products(self, pd_client, params):
+        resp = []
+        try:
+            resp_xml = pd_client.list_matching_products(params)
+            resp = common.xml_to_json(resp_xml.text)
+        except Exception as e:
+            log.error('ListProductError: %s', e)
+        return resp
+
+    def get_product_for_asins(self, pd_client, params):
+        resp = []
+        try:
+            resp_xml = pd_client.get_matching_product(params)
+            resp = common.xml_to_json(resp_xml.text)
+        except Exception as e:
+            log.error('GetProductError: %s', e)
+        return resp
+
+    def get_competitive_pricing_for_asins(self, pd_client, params):
+        resp = []
+        try:
+            resp_xml = pd_client.get_competitive_pricing_for_asin(params)
+            resp = common.xml_to_json(resp_xml.text)
+        except Exception as e:
+            log.error('GetCompetitivePriceError: %s', e)
+        return resp
+
+    def get_lowest_offer_listings_for_asin(self, pd_client, params):
+        resp = []
+        try:
+            resp_xml = pd_client.get_lowest_offer_listings_for_asin(params)
+            resp = common.xml_to_json(resp_xml.text)
+        except Exception as e:
+            log.error('GetLowestOfferListingsForASIN: %s', e)
+        return resp
+
+    def get_lowest_priced_offers_for_asin(self, pd_client, params):
+        resp = []
+        try:
+            resp_xml = pd_client.get_lowest_priced_offers_for_asin(params)
+            resp = common.xml_to_json(resp_xml.text)
+        except Exception as e:
+            log.error('GetLowestPricedOffersForASIN: %s', e)
+        return resp
+
+    def get_myprice_for_asins(self, pd_client, params):
+        resp = []
+        try:
+            resp_xml = pd_client.get_my_price_for_asin(params)
+            resp = common.xml_to_json(resp_xml.text)
+        except Exception as e:
+            log.error('GetMyPriceError: %s', e)
+        return resp
+
+    def get_product_categories_for_asin(self, pd_client, params):
+        resp = []
+        try:
+            resp_xml = pd_client.get_product_categories_for_asin(params)
+            resp = common.xml_to_json(resp_xml.text)
+        except Exception as e:
+            log.error('GetProductCategoriesError: %s', e)
+        return resp
+
+    # 数据库操作
+    def add_product_to_sql(self, tb_name, country, snap_date, json_product):
+        db_table = 'products.{}'.format(tb_name)
+        session = products.DBSession()
+        session.add(eval(db_table)(country, snap_date, json_product))
+        session.commit()
+        session.close()
+
+    def get_asins(self):
+        session = products.DBSession()
+        asins = session.query(products.PubAsin, products.PubAsin.Asin).all()
+        asins = [asin[1] for asin in asins]
+        session.close()
+        return asins
+
+    def get_rank_date(self):
+        session = products.DBSession()
+        field_name = products.AprAsinRank.SnapDate
+        last_date = session.query(products.AprAsinRank, field_name).order_by(desc(field_name)).limit(1).one()
+        session.close()
+        return last_date[1]
+
+    def add_ranks(self, country, snap_date, p_asin, json_rank):
+        session = products.DBSession()
+        for rank in json_rank:
+            session.add(products.AprAsinRank(country, snap_date, p_asin, rank))
+        session.commit()
+        session.close()
+
+    def update_rank(self, country, snap_date, p_asin, json_rank):
+        session = products.DBSession()
+        for rank in json_rank:
+            p_id = rank.get('ProductCategoryId')
+            p_rank = rank.get('Rank')
+            session.query(products.AprAsinRank).filter(and_(and_(and_(
+                                                        products.AprAsinRank.Country == country,
+                                                        products.AprAsinRank.SnapDate == snap_date.split(' ')[0]),
+                                                        products.AprAsinRank.Asin == p_asin),
+                                                        products.AprAsinRank.CategoryId == p_id)) \
+                                               .update({'Rank': p_rank,
+                                                        'LastUpdate': snap_date})
+        session.commit()
+        session.close()
+
+
+
+
+
+if __name__ == '__main__':
+
+    now = datetime.datetime.now()
+    us_time = common.iso_time_to_dsttime(str(now))
+    pd_client = common.get_client(Products, 'us')
+
+    dw_products = DownloadProducts()
+    p_asins = dw_products.get_asins()
+    last_rank_date = dw_products.get_rank_date()
+    log.info('Start download asin_rank... | Asins: %s' % len(p_asins))
+    for asin in p_asins:
+        params = {
+            'ASINList': asin
+        }
+        pd_resp = dw_products.get_competitive_pricing_for_asins(pd_client, params)
+        # 备份文件
+        bak_file_name = '/home/develop/Mws_reports/{f_path}/{f_name}.json'.format(
+                        f_path='GetCompetitivePricingForASIN',
+                        f_name=us_time + '_' + asin)
+        if not os.path.exists(bak_file_name):
+            with open(bak_file_name, 'w', encoding='utf-8') as f:
+                f.write(str(pd_resp))
+            log.info('Baking today asin_rank file...')
+        # 数据入库
+        ranks = pd_resp.get('GetCompetitivePricingForASINResponse') \
+                           .get('GetCompetitivePricingForASINResult') \
+                           .get('Product').get('SalesRankings').get('SalesRank')
+        if us_time.split(' ')[0] != last_rank_date:
+            log.info('Add asin_rank to sql...')
+            dw_products.add_ranks('US', us_time, asin, ranks)
+        else:
+            log.info('Update asin_rank to sql...')
+            dw_products.update_rank('US', us_time, asin, ranks)
+    log.info('End download asin_rank!')
+
+
